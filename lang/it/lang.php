f8f80c5e21ac5b40a72181a353af253f4bb9a014
Diego Pierotto <ita.translations@tiscali.it>
2011-04-30 18:54:54 +0200

    Update Italian translation

--- lang/it/lang.php
+++ lang/it/lang.php
@@ -4,4 +4,10 @@
  * @author Diego Pierotto <ita.translations@tiscali.it>
  */
 
+// settings must be present and set appropriately for the language
+$lang['encoding']   = 'utf-8';
+$lang['direction']  = 'ltr';
+
+// export button
 $lang['view'] = 'Esporta pagina in formato Open Document';
+
---
+++ lang/it/settings.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Italian language file
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Diego Pierotto <ita.translations@tiscali.it>
+ */
+
+// for the configuration manager
+$lang['tpl_dir'] = 'Sotto directory dei modelli nel gestore multimediale';
+$lang['tpl_default'] = 'Modello predefinito';
